143535cdeded687ee0b633c5b627072610a1bbca
Danielle-Luz <daniln2209@gmail.com>
2023-03-05 09:30:54 -0300

    feat: create error class and error handler

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
 import { Application } from "express";
-/*import { errorHandler } from "./errors"; 
-import { usersRouter } from "./routes/users.routes";
-import { categoriesRouter } from "./routes/categories.routes";
-import { realEstateRouter } from "./routes/realEstate.routes";
-import { schedulesRouter } from "./routes/schedules.routes"; */
+import { errorHandler } from "./errors";
+/* import { usersRouter } from "./routes/users.routes"; */
+/* import { categoriesRouter } from "./routes/categories.routes"; */
+/* import { realEstateRouter } from "./routes/realEstate.routes"; */
+/* import { schedulesRouter } from "./routes/schedules.routes"; */
 
-const exprees = require("express");
-const app: Application = exprees();
+const express = require("express");
+const app: Application = express();
 
-app.use(exprees.json());
+app.use(express.json());
 
-/* app.use(errorHandler);
-app.use("/users", usersRouter);
-app.use("/categories", categoriesRouter);
-app.use("/realEstate", realEstateRouter);
-app.use("/schedules", schedulesRouter); */
+app.use(errorHandler);
+// app.use("/users", usersRouter);
+// app.use("/categories", categoriesRouter);
+// app.use("/realEstate", realEstateRouter);
+// app.use("/schedules", schedulesRouter);
 
 export default app;
---
+++ src/errors.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
+
+class AppError extends Error {
+  statusCode: number;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+
+    this.statusCode = statusCode;
+  }
+}
+
+const errorHandler = (
+  error: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).send(error.message);
+  } else if (error instanceof ZodError) {
+    return response.status(400).send({ message: error.flatten().fieldErrors });
+  } else if (error) {
+    return response.status(500).send({ message: "Internal server error" });
+  }
+
+  return next();
+};
+
+export { AppError, errorHandler };
