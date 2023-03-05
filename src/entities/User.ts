572d7e76646740bdba726980255ca78b1451808d
Danielle-Luz <daniln2209@gmail.com>
2023-03-05 12:56:09 -0300

    feat: create user entity

---
+++ src/entities/User.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 45 })
+  name: string;
+
+  @Column({ unique: true, length: 45 })
+  email: string;
+
+  @Column({ default: false })
+  admin: boolean;
+
+  @Column({ length: 120 })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
+
+export default User;
