f77f55c344c72353ad97f54e84313fc97455919f
Danielle-Luz <daniln2209@gmail.com>
2023-03-05 12:07:30 -0300

    feat: add schemas exports in schemas folder's index.ts

---
+++ src/schemas/index.ts
@@ -0,0 +1,11 @@
+import { createCategorieSchema } from "./categorie.schema";
+import { createRealEstateSchema } from "./realEstate.schema";
+import { createSchenduleSchema } from "./schendule.schema";
+import { createUserSchema } from "./user.schema";
+
+export {
+  createCategorieSchema,
+  createRealEstateSchema,
+  createSchenduleSchema,
+  createUserSchema,
+};
