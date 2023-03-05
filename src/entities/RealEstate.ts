448e858cdb1179f5694959bbaf87062a505295a9
Danielle-Luz <daniln2209@gmail.com>
2023-03-05 17:42:14 -0300

    feat: create lacking entities

--- src/entities/RealEstate.ts
+++ src/entities/RealEstate.ts
@@ -1,6 +1,24 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity("real_state")
-class RealEstate {}
+class RealEstate {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: false })
+  sold: boolean;
+
+  @Column({ type: "decimal", precision: 12, scale: 2 })
+  value: number;
+
+  @Column({type: "integer"})
+  size: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
 
 export { RealEstate };
--- src/entities/Schendule.ts
+++ src/entities/Schendule.ts
@@ -1,6 +1,15 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("schendules_users_properties")
-class Schendule {}
+class Schendule {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "datetime" })
+  date: string;
+
+  @Column({ type: "time" })
+  hour: string;
+}
 
 export { Schendule };
