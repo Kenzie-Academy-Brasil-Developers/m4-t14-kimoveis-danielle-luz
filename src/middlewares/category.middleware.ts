4e6d575db2a1bc5b45d3087a3d86b2b5cc030723
Danielle-Luz <daniln2209@gmail.com>
2023-03-06 22:16:35 -0300

    feat: create category functions middlewares interfaces

---
+++ src/middlewares/category.middleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+
+const findCategoryById = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {};
+
+const findCategoryByName = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {};
+
+export { findCategoryById, findCategoryByName };
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { ZodTypeAny } from "zod";
+import { findCategoryById, findCategoryByName } from "./category.middleware";
 import { validateDate, validateTime } from "./schedule.middlewares";
 import {
   findUserByEmailMiddleware,
@@ -30,4 +31,6 @@ export {
   validateTokenMiddleware,
   userIsAdminMiddleware,
   checkUpdatePermission,
+  findCategoryById,
+  findCategoryByName,
 };
