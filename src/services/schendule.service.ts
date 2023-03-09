6ae7ddc6305d31ee1b271702bc54e73c319c8b24
Danielle-Luz <daniln2209@gmail.com>
2023-03-08 21:38:33 -0300

    feat: create find schedule service function

--- src/services/schendule.service.ts
+++ src/services/schendule.service.ts
@@ -1,5 +1,6 @@
 import { AppDataSource } from "../../data-source";
 import { RealEstate, Schedule, User } from "../entities";
+import { AppError } from "../errors";
 import {
   createScheduleInterface,
   realEstateRepo,
@@ -33,4 +34,39 @@ const insertSchenduleService = async (
     .execute();
 
   return createdSchedule;
+};
+
+const findScheduleInTheSameTime = async (
+  newSchedule: createScheduleInterface,
+  userScheduledId: number
+) => {
+  const scheduleRepo: scheduleRepo = AppDataSource.getRepository(Schedule);
+
+  const foundScheduleWithSameTime = await scheduleRepo
+    .createQueryBuilder("schedule")
+    .where("schedule.date = :date", { date: newSchedule.date })
+    .andWhere("schedule.hour = :hour", { hour: newSchedule.hour })
+    .andWhere("schendule.realEstateId = :realEstateId", {
+      realEstateId: newSchedule.realEstateId,
+    })
+    .getExists();
+
+  if (foundScheduleWithSameTime)
+    throw new AppError(
+      409,
+      "Schedule to this real estate at this date and time already exists"
+    );
+
+  const findScheduleWithSameUserAndTime = await scheduleRepo
+    .createQueryBuilder()
+    .where("schedule.date = :date", { date: newSchedule.date })
+    .andWhere("schedule.hour = :hour", { hour: newSchedule.hour })
+    .andWhere("schedule.userId = :userId", { userId: userScheduledId })
+    .getExists();
+
+  if (findScheduleWithSameUserAndTime)
+    throw new AppError(
+      409,
+      "User schedule to this real estate at this date and time already exists"
+    );
 };
